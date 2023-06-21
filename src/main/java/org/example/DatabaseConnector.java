2a7ae78be3f7df09375dd1052eef2f4b705d40da
kayonelee <klee93@live.com>
2023-06-21 12:31:31 -0400

    commit

---
+++ src/main/java/org/example/DatabaseConnector.java
@@ -0,0 +1,47 @@
+package org.example;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.*;
+public class DatabaseConnector {
+    public Connection connect() {
+        Connection connection = null;
+        try {
+            String url = "jdbc:sqlite:/Users/klee9/CTAC/Bookstore";
+            connection = DriverManager.getConnection(url);
+            System.out.println("Successfully connected to the database!");
+        } catch (SQLException e) {
+            System.out.println("Error connecting to the database.");
+            e.printStackTrace();
+        }
+        return connection;
+    }
+    public void selectAllBooks() {
+        Connection connection = null;
+        Statement stmt = null;
+        ResultSet rs = null;
+        try {
+            connection = this.connect();
+            String sql = "SELECT * FROM books";
+            stmt = connection.createStatement();
+            rs = stmt.executeQuery(sql);
+
+            while (rs.next()) {
+                System.out.println(rs.getString("title") + "\t" +
+                        rs.getString("author") + "\t" +
+                        rs.getDouble("price"));
+            }
+        } catch (SQLException e) {
+            System.out.println("Error executing SELECT statement");
+            e.printStackTrace();
+        } finally {
+            try {
+                if (rs != null) rs.close();
+                if (stmt != null) stmt.close();
+                if (connection != null) connection.close();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
